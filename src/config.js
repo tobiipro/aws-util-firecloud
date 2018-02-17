b106ffcd7018471cb05c1526ef4ed207b212e04f
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-02-17 23:58:18 +0100

    rename getAwsConfig to get. redundant

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import logger from './logger';
 
-export let getAwsConfig = function({env}) {
+export let get = function({env}) {
   let region = env.AWS_REGION;
 
   // TODO should probably handle credentials, etc.
