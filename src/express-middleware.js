139f3ef631329b153b3b556915ce7a46cd76ab84
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-14 22:49:46 +0100

    prioritize a response error only in envs with TRACE log level. crash & start clean otherwise

--- src/express-middleware.js
+++ src/express-middleware.js
@@ -14,19 +14,38 @@ export let bootstrap = function(fn) {
       await fn(...args);
     } catch (err) {
       res.log.error({err});
-      if (res.headersSent) {
-        return;
+
+      if (res.ctx.log.level() > res.ctx.log.resolveLevel('TRACE')) {
+        // if no trace is desired, we'd better just exit
+        // eslint-disable-next-line no-process-exit
+        process.exit(1);
       }
 
-      let trace;
-      if (res.ctx.log.level() <= res.ctx.log.resolveLevel('TRACE')) {
-        trace = err.stack ? _.split(err.stack, '\n') : err;
+      if (res.headersSent) {
+        // we cannot amend the response anymore
+        // eslint-disable-next-line no-process-exit
+        process.exit(1);
       }
+
+      // NOTE we prioritize responding with a body that includes error details
+      // and exits on the next call.
+      // The exit is required, in order to guarantee a clean state,
+      // as opposed to the borked one in which the error was triggered.
+      let trace = err.stack ? _.split(err.stack, '\n') : err;
 
       res.ctx.callbackWaitsForEmptyEventLoop = false;
+      _.delay(function() {
+        // this will run on the next call,
+        // because calling res.send will immediately freeze the process,
+        // only to be unfrozen by the next call
+        // eslint-disable-next-line no-process-exit
+        process.exit(1);
+      }, 1000);
 
       res.status(500);
-      res.set('content-type', 'application/problem+json');
+      res.set({
+        'content-type': 'application/problem+json'
+      });
       res.send({
         type: 'about:blank',
         title: 'Internal Server Error',
@@ -35,8 +54,6 @@ export let bootstrap = function(fn) {
         renderer: 'lambda-util',
         trace
       });
-
-      // don't process.exit()
     }
   });
 };
