0af9953cd0a0e6ef76389e806e1c404f16f909b0
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-24 19:43:27 +0100

    add metric utils to cloudwatch

--- src/cloudwatch.js
+++ src/cloudwatch.js
@@ -27,4 +27,123 @@ export let listAllMetrics = async function({
   return exports.listAllMetrics({Token: NextToken, Metrics});
 };
 
+/*
+  metrics = [{
+    Namespace: '...',
+    MetricName: '...',
+    Dimensions: [{
+      Name: '...',
+      Value: '...'
+    }]
+  }];
+
+  dimensionMetrics = [{
+    Namespace: '...',
+    MetricName: '...',
+    Dimension: { // only 1 dimension
+      Name: '...',
+      Value: '...'
+    }
+  }];
+*/
+
+export let metricsToDimensionMetrics = function(metrics) {
+  let dimensionMetrics = [];
+
+  _.forEach(metrics, function(metric) {
+    _.forEach(metric.Dimensions, function(dimension) {
+      let dimensionMetric = _.cloneDeep(metric);
+      _.unset(metric, 'Dimensions');
+      dimensionMetric.Dimension = dimension;
+      dimensionMetrics.push(dimensionMetric);
+    });
+  });
+
+  return dimensionMetrics;
+};
+
+/*
+  dmDatapoint = {
+    region: '...'
+    namespace: '...',
+    dimension_name: '...',
+    dimension_value: '...',
+    start_time: '...',
+    end_time: '...',
+    period: ...,
+    label: '...',
+    datapoint: {
+      timestamp: '...',
+      <statistic>: ...,
+      unit: '...'
+    }
+  }
+*/
+
+export let datapointToDmDatapoint = function({dmDatapointTpl, datapoint}) {
+  let dmDatapoint = _.cloneDeep(dmDatapointTpl);
+
+  datapoint.Timestamp = new Date(datapoint.timestamp).toISOString();
+  datapoint = _.mapKeys(datapoint, function(_value, key) {
+    return _.snakeCase(key);
+  });
+  dmDatapoint.datapoint = datapoint;
+
+  return dmDatapoint;
+};
+
+export let getDimensionMetricDatapoints = async function({
+  cloudwatch,
+  dimensionMetric,
+  StartTime,
+  EndTime,
+  Period
+}) {
+  let {
+    Namespace,
+    Dimension,
+    MetricName
+  } = dimensionMetric;
+
+  let {
+    Datapoints,
+    Label
+  } = await cloudwatch.getMetricStatistics({
+    Namespace,
+    Dimensions: [
+      Dimension
+    ],
+    MetricName,
+    StartTime: new Date(StartTime).toISOString(),
+    EndTime: new Date(EndTime).toISOString(),
+    Period,
+    Statistics: [
+      'Average',
+      'Maximum',
+      'Minimum',
+      'SampleCount',
+      'Sum'
+    ]
+  }).promise();
+
+  let dmDatapointTpl = {
+    region: cloudwatch.config.region,
+    namespace: Namespace,
+    dimension_name: Dimension.Name,
+    dimension_value: Dimension.Value,
+    metric_name: MetricName,
+    start_time: StartTime.toISOString(),
+    end_time: EndTime.toISOString(),
+    period: Period,
+    label: Label,
+    datapoint: {}
+  };
+
+  let dmDatapoints = _.map(Datapoints, function(datapoint) {
+    return datapointToDmDatapoint({dmDatapointTpl, datapoint});
+  });
+
+  return dmDatapoints;
+};
+
 export default exports;
