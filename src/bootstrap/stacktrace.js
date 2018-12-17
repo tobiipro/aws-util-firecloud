f182dff9f2f13217156d69e4bc8529c9a9c169fa
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-17 08:27:25 +0100

    try to use bluebird

--- src/bootstrap/stacktrace.js
+++ src/bootstrap/stacktrace.js
@@ -1,9 +1,18 @@
-import Promise from 'bluebird';
-
 // override native Promise to get better stacktraces
-global.Promise = Promise;
-Promise.config({
-  warnings: true,
-  longStackTraces: true
-});
+try {
+  let originalPromise = global.Promise;
+
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  let Promise = require('bluebird');
+  global.Promise = Promise;
+
+  Promise.original = originalPromise;
+  Promise.config({
+    warnings: true,
+    longStackTraces: true
+  });
+} catch (_err) {
+  // console.log(_err);
+}
+
 Error.stackTraceLimit = Infinity;
