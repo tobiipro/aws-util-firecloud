ed22bef4acc033882566e4d4494a583ed38875eb
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-17 08:27:46 +0100

    remove bluebird because it won't cover async/await anyway

--- src/bootstrap/stacktrace.js
+++ src/bootstrap/stacktrace.js
@@ -1,18 +1,1 @@
-// override native Promise to get better stacktraces
-try {
-  let originalPromise = global.Promise;
-
-  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
-  let Promise = require('bluebird');
-  global.Promise = Promise;
-
-  Promise.original = originalPromise;
-  Promise.config({
-    warnings: true,
-    longStackTraces: true
-  });
-} catch (_err) {
-  // console.log(_err);
-}
-
 Error.stackTraceLimit = Infinity;
