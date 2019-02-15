5142164a633a163d349b2f4f12b345616d2f8456
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-15 22:21:22 +0100

    fixup! eslint-config-firecloud@0.5.0

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-unassigned-import
 import './aws-sdk-logger';
+// eslint-disable-next-line import/no-unassigned-import
 import './exit-on-unhandled-rejection';
+// eslint-disable-next-line import/no-unassigned-import
 import './stacktrace';
