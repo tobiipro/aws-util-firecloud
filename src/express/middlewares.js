0f88b84b0d19005465287908e7aefdce367006c2
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-18 22:51:17 +0100

    fix middleware signature

--- src/express/middlewares.js
+++ src/express/middlewares.js
@@ -2,24 +2,29 @@ import _ from 'lodash-firecloud';
 import reqMixins from './req-mixins';
 import resMixins from './res-mixins';
 
-export let applyMixins = function(req, res, next) {
-  _.forEach(reqMixins, function(fn, name) {
-    req[name] = _.bind(fn, req);
-  });
+export let applyMixins = function() {
+  return function(req, res, next) {
+    _.forEach(reqMixins, function(fn, name) {
+      req[name] = _.bind(fn, req);
+    });
 
-  res.oldSend = res.send; // required by the res.send mixin
-  _.forEach(resMixins, function(fn, name) {
-    res[name] = _.bind(fn, res);
-  });
+    res.oldSend = res.send; // required by the res.send mixin
+    _.forEach(resMixins, function(fn, name) {
+      res[name] = _.bind(fn, res);
+    });
 
-  next();
+    next();
+  };
 };
 
-export let xForward = function(req, _res, next) {
-  req.headers = _.mapKeys(req.headers, function(_value, key) {
-    return _.replace(key, /^X-Forward-/, '');
-  });
-  next();
+export let xForward = function() {
+  return function(req, _res, next) {
+    req.headers = _.mapKeys(req.headers, function(_value, key) {
+      return _.replace(key, /^X-Forward-/, '');
+    });
+
+    next();
+  };
 };
 
 export default exports;
