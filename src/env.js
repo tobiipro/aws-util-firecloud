f7e6a89be6bc4f528cc37009342966d9c221a280
Andrei Neculau <andrei.neculau@gmail.com>
2019-03-04 22:37:19 +0100

    use _.safeProxy

--- src/env.js
+++ src/env.js
@@ -1,23 +1,11 @@
 import _ from 'lodash-firecloud';
 
-export let envProxy = function({env}) {
-  // eslint-disable-next-line fp/no-proxy
-  return new Proxy(env, {
-    get: function(target, property, _receiver) {
-      if (property === '_') {
-        return target;
-      }
-      if (!_.isString(target[property])) {
-        throw new Error(`env.${property} is undefined.`);
-      }
-      return target[property];
-    }
-  });
-};
+// FIXME backward compat
+export let envProxy = _.safeProxy;
 
-export let current = envProxy({env: process.env});
+export let current = _.safeProxy(process.env);
 
-// backward compat alias
+// FIXME backward compat alias
 export let env = current;
 
 export default current;
